8d5ec91b585127905b1dbe20a784542605d3b9b6
cesc <ccunig@gmail.com>
2023-06-20 22:05:41 +0100

    update mc_control to take from rl_agent

---
+++ models/base_rl_agent.py
@@ -0,0 +1,31 @@
+from abc import abstractmethod
+
+from env.gelateria_env import GelateriaEnv
+from utils.config import OptimiserConfig
+
+
+class RLAgent:
+    def __init__(self, env: GelateriaEnv, config: OptimiserConfig, name: str):
+        self._env = env
+        self._config = config
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def config(self):
+        return self._config
+
+    @abstractmethod
+    def train(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def save(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def load(self):
+        raise NotImplementedError
